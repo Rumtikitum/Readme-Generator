19c427a1d8e770636bc862408f865a8cf23d5af7
Rumtikitum <rt.student.only@gmail.com>
2021-06-17 02:38:18 -0700

    half way complete

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const promptUser = () => {
         name: 'about',
         message: 'Provide some information as to why you chose the project:',
         when: ({ confirmWhy }) => confirmWhy
-      }
+      },
       //=====================================================================
       {
         type: 'confirm',
@@ -61,101 +61,12 @@ const promptUser = () => {
     ]);
   };
   
-  const promptProject = portfolioData => {
-    console.log(`
-  =================
-  Add a New Project
-  =================
-  `);
-  
-    // If there's no 'projects' array property, create one
-    if (!portfolioData.projects) {
-      portfolioData.projects = [];
-    }
-    return inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'name',
-          message: 'What is the name of your project? (Required)',
-          validate: nameInput => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log('You need to enter a project name!');
-              return false;
-            }
-          }
-        },
-        {
-          type: 'input',
-          name: 'description',
-          message: 'Provide a description of the project (Required)',
-          validate: descriptionInput => {
-            if (descriptionInput) {
-              return true;
-            } else {
-              console.log('You need to enter a project description!');
-              return false;
-            }
-          }
-        },
-        {
-          type: 'checkbox',
-          name: 'languages',
-          message: 'What did you this project with? (Check all that apply)',
-          choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-        },
-        {
-          type: 'input',
-          name: 'link',
-          message: 'Enter the GitHub link to your project. (Required)',
-          validate: linkInput => {
-            if (linkInput) {
-              return true;
-            } else {
-              console.log('You need to enter a project GitHub link!');
-              return false;
-            }
-          }
-        },
-        {
-          type: 'confirm',
-          name: 'feature',
-          message: 'Would you like to feature this project?',
-          default: false
-        },
-        {
-          type: 'confirm',
-          name: 'confirmAddProject',
-          message: 'Would you like to enter another project?',
-          default: false
-        }
-      ])
-      .then(projectData => {
-        portfolioData.projects.push(projectData);
-        if (projectData.confirmAddProject) {
-          return promptProject(portfolioData);
-        } else {
-          return portfolioData;
-        }
-      });
-  };
-  
   promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-      return generatePage(portfolioData);
-    })
-    .then(pageHTML => {
-      return writeFile(pageHTML);
+    .then(pageREADME => {
+      return writeFile(pageREADME);
     })
     .then(writeFileResponse => {
       console.log(writeFileResponse);
-      return copyFile();
-    })
-    .then(copyFileResponse => {
-      console.log(copyFileResponse);
     })
     .catch(err => {
       console.log(err);
