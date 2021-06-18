bdb8d51e2281ba3c57ccb76e54b06e5744991972
Rumtikitum <rt.student.only@gmail.com>
2021-06-18 01:46:02 -0700

    updating code as needed

---
+++ README.md
@@ -0,0 +1,27 @@
+## Title
+
+README Generator
+
+## Description
+
+This project is an engine people can use to quickly generate README.md files.
+
+## Usage
+
+People can utilize this project to simplify and expedite their workflow.
+
+## People
+
+undefined
+
+## Questions
+
+Questions? Reach out to me here:
+
+ ### Github: [github.com/Rumtikitum](https://github.com/Rumtikitum)
+
+ ### Email: [rtauc001@gmail.com](mailto:rtauc001@gmail.com?subject=[GitHub])
+
+## Application Image
+
+ ![](./Picture.JPG)
--- app.js
+++ app.js
@@ -1,74 +1,66 @@
-const { writeFile } = require('./utils/generate-readme.js');
-const inquirer = require('inquirer');
-const generatePage = require('./src/page-template');
+const inquirer = require("inquirer");
+const fs = require('fs');
+let location = './README.md';
+let $template = "";
 
-const promptUser = () => {
-    return inquirer.prompt([
-      //---------------------------------------------------------------
-        {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project? (Required)',
-        validate: titleInput => {
-          if (titleInput) {
-            return true;
-          } else {
-            console.log('Please enter your title name!');
-            return false;
-          }
-        }
-      },
-      //----------------------------------------------------------------
+// Creates inquirer questions
+function getInfo() {
+  inquirer
+    .prompt([
       {
-        type: 'input',
-        name: 'description',
-        message: 'Enter a short description of your project. What was the purpose? What does it do? (Required)',
-        validate: descriptionInput => {
-          if (descriptionInput) {
-            return true;
-          } else {
-            console.log('Please enter your description!');
-            return false;
-          }
-        }
-      },
-      //====================================================================
-      {
-        type: 'confirm',
-        name: 'confirmWhy',
-        message: 'Would you like to share why you chose to make your project?',
-        default: true
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title"
       },
       {
-        type: 'input',
-        name: 'about',
-        message: 'Provide some information as to why you chose the project:',
-        when: ({ confirmWhy }) => confirmWhy
-      },
-      //=====================================================================
-      {
-        type: 'confirm',
-        name: 'confirmWhat',
-        message: 'Would you like to share what you learned from this project?',
-        default: true
+        type: "input",
+        message: "Enter a description for your project.",
+        name: "description"
       },
       {
-        type: 'input',
-        name: 'about',
-        message: 'Provide some information as to what you learned from the project:',
-        when: ({ confirmWhat }) => confirmWhat
+        type: "input",
+        message: "Enter an explanation of how people are to use the project. How does this benefit them? The purpose.",
+        name: "usage"
+      },
+      {
+        type: "input",
+        message: "Who played a role in making this project? Include yourself and other contributors and partners",
+        name: "People"
+      },
+      {
+        type: "input",
+        message: "what is your Github username?",
+        name: "username"
+      },
+      {
+        type: "input",
+        message: "what is your email address?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "Please include the image that is locally stored ![](...). No need to add anything except the file location. '![](...)' is already included.",
+        name: "image"
       }
-    ]);
-  };
-  
-  promptUser()
-    .then(pageREADME => {
-      return writeFile(pageREADME);
-    })
-    .then(writeFileResponse => {
-      console.log(writeFileResponse);
-    })
-    .catch(err => {
-      console.log(err);
+    ])
+    // Receives response from questions
+    .then(function (response) {
+      // Creates the template for the readme
+      $template += `## Title\n\n${response.title}\n\n`;      
+      $template += `## Description\n\n${response.description}\n\n`;
+      $template += `## Usage\n\n${response.usage}\n\n`;
+      $template += `## People\n\n${response.people}\n\n`;
+      $template += `## Questions\n\nQuestions? Reach out to me here:\n\n ### Github: [github.com/${response.username}](https://github.com/${response.username})\n\n ### Email: [${response.email}](mailto:${response.email}?subject=[GitHub])\n\n`;
+      $template += `## Application Image\n\n ![](${response.image})`;
+      // Writes the created template to README.md file
+      fs.writeFile(location, $template, function (err) {
+        if (err) {
+          reject(err);
+          return;
+        }
+      });
     });
-  
+
+};
+
+getInfo()
